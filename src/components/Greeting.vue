651720d6983f6d427f5c6f9b21bc55c0c7fbdde2
chenjiefeng <chen233jf@outlook.com>
2025-06-11 17:48:15 +0800

    chore: 集成 Vue3

---
+++ src/components/Greeting.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h3>{{ greeting }}！感谢来访！</h3>
+    <button @click="setGreeting">新的欢迎语</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const { messages } = defineProps<{
+  messages: string[]
+}>()
+
+const greeting = ref(messages[0])
+
+const randomMessage = () => {
+  return messages[Math.floor(Math.random() * messages.length)]
+}
+
+const setGreeting = () => {
+  greeting.value = randomMessage()
+}
+</script>
