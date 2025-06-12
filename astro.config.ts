1b8db6f09cdf32c0a26ff36371b2e35e59a75ce5
chenjiefeng <chen233jf@outlook.com>
2025-06-12 17:10:00 +0800

    refactor: add unocss integration

---
+++ astro.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'astro/config'
+
+import vue from '@astrojs/vue'
+import UnoCSS from 'unocss/astro'
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    vue(),
+    UnoCSS({
+      injectReset: true,
+    }),
+  ],
+})
